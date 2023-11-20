f3bf943b7207b400078f698a0ef4b1506bb2eddf
GitHub Action <action@github.com>
2023-11-20 18:41:01 +0000

    Update version to 1.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.1.4'
+VERSION = '1.2.0'
 DESCRIPTION = 'Modular, mesh, multi-device LoRa Content Transfer Protocol'
 
 # Setting up
